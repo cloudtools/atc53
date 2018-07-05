fd33147aab13f6d365902d0b84fbb33a282de7fe
Tony Tiger <thatrascaltiger@gmail.com>
2018-07-05 16:18:56 +0100

    Adding bit for long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+def readme():
+    with open('README.rst') as f:
+    return f.read()
+
 setup(
     name='atc53',
     version='0.0.1',
     description='AWS Traffic Policy Document creation library',
+    long_description=readme(),
     author='Tony Tiger',
     author_email='thatrascaltiger@gmail.com',
     url='https://github.com/tigertoes/tpd',
